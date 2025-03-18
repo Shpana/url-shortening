4d7a2f53fc814ff945e83a4617be1115edc1feec
Shpana <romankozell@yandex.ru>
2025-03-18 22:45:44 +0300

    feat shorten: implement api

---
+++ URLShortening/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URLShortening.Models.Internal;
+
+namespace URLShortening;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<ShortenedUrlItem> ShortenedUrls { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+        Database.Migrate();
+    } 
+}
---
+++ URLShortening/Models/Responses/ShortenedUrlResponse.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+using URLShortening.Models.Internal;
+
+namespace URLShortening.Models.Responses;
+
+public class ShortenedUrlResponse
+{
+    public int Id { get; set; }
+    [JsonPropertyName("url")]
+    public string OriginalUrl { get; set; }
+    [JsonPropertyName("shortCode")]
+    public string ShortenedUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
+
+public static class ShortenedUrlResponseMapping
+{
+    public static ShortenedUrlResponse ToShortenedUrlResponse(this ShortenedUrlItem item)
+    {
+        return new ShortenedUrlResponse()
+        {
+            Id = item.Id,
+            OriginalUrl = item.OriginalUrl,
+            ShortenedUrl = item.ShortenedUrl,
+            CreatedAt = item.CreatedAt,
+            UpdatedAt = item.UpdatedAt
+        };
+    }
+}
